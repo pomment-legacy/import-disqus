a1f751fdf86e4e6c8c679dc00cd90390f4868f43
tcdw <tcdw2011@gmail.com>
2019-09-25 22:04:30 +0800

    初步功能

--- web/index.js
+++ web/index.js
@@ -1,2 +1,12 @@
 import parseDisqusXML from '../lib/parse';
 import './scss/index.scss';
+
+const file = document.getElementById('content');
+file.addEventListener('change', async () => {
+    const fileInfo = file.files[0];
+    const fileReader = new FileReader();
+    fileReader.onload = (e) => {
+        console.log(parseDisqusXML(e.target.result));
+    };
+    fileReader.readAsText(fileInfo, 'utf-8');
+});
